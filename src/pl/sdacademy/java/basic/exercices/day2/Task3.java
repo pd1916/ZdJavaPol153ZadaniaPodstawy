b74bdc6dccacd6e878d96ce520a5d9892355effb
Pawel Dudko <paweldudko@outlook.com>
2023-12-10 11:36:23 +0100

    solution for day2_task3

---
+++ src/pl/sdacademy/java/basic/exercices/day2/Task3.java
@@ -0,0 +1,20 @@
+package pl.sdacademy.java.basic.exercices.day2;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        String input = "Ala ma kota";
+        String word = "ma,";
+        int result = getIndex(input, word);
+        System.out.println(result);
+    }
+
+    private static int getIndex(String input, String word) {
+        if(StringValidator.isValid(input) && StringValidator.isValid(word)) {
+            //if(input.contains(word)) {
+                return input.indexOf(word);
+            //}
+        }
+        return -1;
+    }
+}
